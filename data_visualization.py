a67199909326542cd99af8dc6456a609ba089910
volodyslav <vladver1211@gmail.com>
2024-05-16 20:36:48 +0300

    add: radio button for different plots

--- data_visualization.py
+++ data_visualization.py
@@ -1,11 +1,9 @@
 import flet as ft
 import numpy as np
 import pandas as pd
-import matplotlib
 import matplotlib.pyplot as plt
 from flet.matplotlib_chart import MatplotlibChart
-
-matplotlib.use("svg")
+from PIL import Image
 
 
 class DataVisualization:
@@ -14,23 +12,35 @@ class DataVisualization:
         self.page = page
         self.data_frame = None
 
-        self.fig, self.ax = plt.subplots()
-        self.row_plot = ft.Row()
-        self.plot_line_btn = ft.TextButton("Plot a line", on_click=self.draw_line)
 
-        self.row_plot.controls.append(self.plot_line_btn)
+        self.row_plot = ft.Column()
+        # Select a plotting
+        self.label_plot = ft.Text("Choose a plot: ")
+
+        self.plot_radio = ft.RadioGroup(content=ft.Column([
+            ft.Radio(value="line", label="Line"),
+            ft.Radio(value="scatter", label="Scatter"),
+            ft.Radio(value="bar", label="Bar")]))
+
+        self.submit_draw = ft.TextButton("Draw")
+
+        self.row_plot.controls.append(self.label_plot)
+        self.row_plot.controls.append(self.plot_radio)
+        self.row_plot.controls.append(self.submit_draw)
 
     def update_data_frame(self, df):
         """Update df from file picker helper"""
         self.data_frame = df
 
+
     def draw_line(self, e):
         if self.data_frame is not None:
             try:
-                self.ax.clear()
-                self.ax.plot(self.data_frame.iloc[:, 0], self.data_frame.iloc[:, 1])
-                self.row_plot.controls.clear()
-                self.row_plot.controls.append(MatplotlibChart(self.fig, expand=True))
+                fig, ax = plt.subplots()
+                ax.plot(self.data_frame.iloc[:, 0], self.data_frame.iloc[:, 1])
+                fig.savefig("line_plot.png")
+                with Image.open("line_plot.png") as image:
+                    image.show()
                 self.row_plot.update()
             except Exception as e:
                 print("An error occurred:", e)
