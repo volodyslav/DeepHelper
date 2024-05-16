170394c0c78d069143593ccffd6619ccd37f2247
volodyslav <vladver1211@gmail.com>
2024-05-16 20:59:00 +0300

    add: column for plot settings

--- data_visualization.py
+++ data_visualization.py
@@ -2,8 +2,9 @@ import flet as ft
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from flet.matplotlib_chart import MatplotlibChart
 from PIL import Image
+import webbrowser
+import threading
 
 
 class DataVisualization:
@@ -12,8 +13,11 @@ class DataVisualization:
         self.page = page
         self.data_frame = None
 
+        # Column for radio selection
+        self.column_plot = ft.Column(alignment=ft.MainAxisAlignment.CENTER)
+        # Column for selecting right settings for a plot
+        self.column_settings = ft.Column(alignment=ft.MainAxisAlignment.CENTER)
 
-        self.row_plot = ft.Column()
         # Select a plotting
         self.label_plot = ft.Text("Choose a plot: ")
 
@@ -22,26 +26,36 @@ class DataVisualization:
             ft.Radio(value="scatter", label="Scatter"),
             ft.Radio(value="bar", label="Bar")]))
 
-        self.submit_draw = ft.TextButton("Draw")
+        self.submit_draw = ft.TextButton("Submit", on_click=self.select_radio, disabled=True)
 
-        self.row_plot.controls.append(self.label_plot)
-        self.row_plot.controls.append(self.plot_radio)
-        self.row_plot.controls.append(self.submit_draw)
+        self.column_plot.controls.append(self.label_plot)
+        self.column_plot.controls.append(self.plot_radio)
+        self.column_plot.controls.append(self.submit_draw)
 
     def update_data_frame(self, df):
         """Update df from file picker helper"""
         self.data_frame = df
+        self.submit_draw.disabled = False
+        self.submit_draw.update()
 
+    def select_radio(self, e):
+        if self.plot_radio.value == "line":
+            self.draw_line()
 
-    def draw_line(self, e):
+    def draw_line(self):
+        if self.data_frame is not None:
+            threading.Thread(target=self.plot_data_frame).start()
+        else:
+            print("Data frame is None.")
+
+    def plot_data_frame(self):
         if self.data_frame is not None:
             try:
                 fig, ax = plt.subplots()
                 ax.plot(self.data_frame.iloc[:, 0], self.data_frame.iloc[:, 1])
                 fig.savefig("line_plot.png")
-                with Image.open("line_plot.png") as image:
-                    image.show()
-                self.row_plot.update()
+                webbrowser.open("line_plot.png")
+                self.column_plot.update()
             except Exception as e:
                 print("An error occurred:", e)
         else:
